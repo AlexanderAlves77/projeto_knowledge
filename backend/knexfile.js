3aa3d149693b9f074040423c98fb5ab19a6014f8
Alexander Alves <vendas77.rj@gmail.com>
2022-02-17 16:27:21 -0300

    Modificando as regras do Banco de Dados

---
+++ backend/knexfile.js
@@ -0,0 +1,15 @@
+module.exports  =  {      
+    client : 'mysql2' , 
+      connection : { 
+        database : 'knowledge' , 
+        user : 'root' , 
+        password : 'rska2022' 
+      }, 
+      pool : { 
+        min : 2 , 
+        max : 10 
+      }, 
+      migrações: { 
+        tableName : 'knex_migrations' 
+    } 
+} 
