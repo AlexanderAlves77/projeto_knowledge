c39569bc6f00638d28aa54c29757d38afa0cc065
Alexander Alves <vendas77.rj@gmail.com>
2022-02-19 17:35:40 -0300

    Criando a autorização do login

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,6 +1,10 @@
 const { get } = require("express/lib/response")
 
 module.exports = app => {
+    app.post("/signup", app.api.user.save)
+    app.post("/signin", app.api.auth.signin)
+    app.post("/validateToken", app.api.auth.validateToken)
+
     app.route("/users")
         .post(app.api.user.save)
         .get(app.api.user.get)
