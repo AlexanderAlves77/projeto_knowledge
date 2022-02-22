cf8b81c02119695b2da22c5f722fec1735dfbc60
Alexander Alves <vendas77.rj@gmail.com>
2022-02-22 13:36:29 -0300

    Componente Home e páginas administrativas

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORARIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsZXhhbmRlciBBbHZlcyIsImVtYWlsIjoiYWVjYTc3OTFAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTY0NTU0MzAyMSwiZXhwIjoxNjQ1ODAyMjIxfQ.y17WdnIvvviU9IpYzeRY3B9Clc3-BO4fvBxnp5-bwLQ'
+
 new Vue({
   store,
   router,
