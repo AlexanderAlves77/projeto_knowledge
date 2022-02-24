7fd9d72eace04a29629957518c2a8c6968776e2f
Alexander Alves <vendas77.rj@gmail.com>
2022-02-24 16:00:45 -0300

    Adicionando o highlight no código e responsividade do menu

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,13 @@ import App from './App'
 
 import './config/bootstrap'
 import './config/msgs'
+import './config/axios'
+import './config/mq'
+
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
-
-// TEMPORARIO!
-// require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsZXhhbmRlciBBbHZlcyIsImVtYWlsIjoiYWVjYTc3OTFAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTY0NTU0MzAyMSwiZXhwIjoxNjQ1ODAyMjIxfQ.y17WdnIvvviU9IpYzeRY3B9Clc3-BO4fvBxnp5-bwLQ'
 
 new Vue({
   store,
